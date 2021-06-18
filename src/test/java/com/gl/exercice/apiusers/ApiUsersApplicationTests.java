0d130cf69a9377549bfc54c9cc0afb31e1139d5c
Juan Pablo <luduenajp@gmail.com>
2021-06-18 18:38:04 -0300

    first commit

---
+++ src/test/java/com/gl/exercice/apiusers/ApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.exercice.apiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
